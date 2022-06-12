233c2140f299dddf540a15642e0a552c0b0484ae
Your Name <you@example.com>
2022-06-12 16:10:47 +0400

    override login function

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Shop\CartItem;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class LoginController extends Controller
 {
@@ -31,7 +34,7 @@ class LoginController extends Controller
     //protected $redirectTo = RouteServiceProvider::HOME;
     //protected $redirectTo = '/';
     public function redirectTo() {
-        $user = User::find(Auth::user()->id); 
+        $user = User::find(Auth::user()->id);
         if($user->hasRole(['super_admin']))
                     return '/dashboard';
         if($user->hasRole(['customer']))
@@ -47,4 +50,56 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            if ($request->hasSession()) {
+                $request->session()->put('auth.password_confirmed_at', time());
+            }
+
+            // add cart session details to user logged in cart
+            $session_cart = Session::get('cart');
+            if($session_cart){    // there is cart in the session
+                $user_loggedin = User::with('cart.cartItems')->find(Auth::user()->id);
+                // delete old cart items if exist
+                if($user_loggedin->cart->cartItems->count()>0)
+                    foreach($user_loggedin->cart->cartItems as $old_item){
+                        $old_item->delete();
+                    }
+                // add session cart to user cart
+                foreach($session_cart as $item){
+                        $cartItem = CartItem::create([
+                            'cart_id' => $user_loggedin->cart->id,
+                            'product_id' => $item['product_id'],
+                            'quantity' => $item['quantity'],
+                        ]);
+                }
+            }
+            Session::forget('cart');
+
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+
 }
